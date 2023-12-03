7c1b5471950d8bb2dd47e4e7b68cc520ad6a5ca0
Tridib Ghosh <86187166+Tridib11@users.noreply.github.com>
2023-12-03 23:30:18 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes_and_ECMA_Standards.js
@@ -0,0 +1,21 @@
+"use strict" //treats all JS code as newer Version
+
+//alert(3+3) //we are using nodjs, not browser
+
+let name="Tridib"
+let age=18
+let isLoggedIn=false
+// Primitive datatypes
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>standalone vlaue
+// undefined => value is not assigned yet
+// symbol => usnique
+
+
+console.log(typeof "tridib") // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
