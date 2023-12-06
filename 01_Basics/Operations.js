0c2637f09ad60db1567b825daf6f31111aed5883
Tridib Ghosh <86187166+Tridib11@users.noreply.github.com>
2023-12-06 10:48:46 +0000

    Operations file added

---
+++ 01_Basics/Operations.js
@@ -0,0 +1,35 @@
+let value=3
+let negValue=-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1="Tridib"
+// let str2=" Ghosh"
+// let str3= str1+str2
+// console.log(str3);
+
+
+// console.log("1"+2); // => 12
+// console.log(1+"2"); // => 12
+// console.log("1"+2+2); // => 122
+// console.log(1+2+"2");// => 32
+
+/* so in JS when there is string in the first all the things will be converted to string eg- console.log("1"+2); gives 12 and when the string is in the last the operations will be perfored first and then it will become a string eg - console.log(1+2+"2"); gives 32  */
+
+
+console.log(+true); //1
+console.log(+"");//0
+
+let num1,num2,num3=2+2
+
+
+let gameCounter=100
+// gameCounter++
+console.log(gameCounter++);// first operations then when again used will be 100
+// console.log(++gameCounter); // first add then will be printed 101
