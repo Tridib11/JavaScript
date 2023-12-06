503c582b546f5d9a6af15861d80d4284cce6fe52
Tridib Ghosh <86187166+Tridib11@users.noreply.github.com>
2023-12-06 10:32:07 +0000

    ConversionofDatatypes

--- 01_Basics/conversionoperations.js
+++ 01_Basics/conversionoperations.js
@@ -3,4 +3,22 @@ console.log(typeof score);
 
 let valueInNunber=Number(score)
 console.log(typeof valueInNunber);
-console.log(valueInNunber);//NaN
+console.log(valueInNunber);//NaN 
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true;
+// 0 => false;
+// "" =>false;
+//"Tridib" => true;
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
