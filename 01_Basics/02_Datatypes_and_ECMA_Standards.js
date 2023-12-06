9cb56d483ca996e49e851b1857ff582e10d9deea
Tridib Ghosh <86187166+Tridib11@users.noreply.github.com>
2023-12-06 18:32:15 +0000

    Datatypes file added

--- 01_Basics/02_Datatypes_and_ECMA_Standards.js
+++ 01_Basics/02_Datatypes_and_ECMA_Standards.js
@@ -1,3 +1,13 @@
+/*
+There are 7 primitive data types:
+string
+number
+bigint
+boolean
+undefined
+symbol
+null
+*/
 "use strict" //treats all JS code as newer Version
 
 //alert(3+3) //we are using nodjs, not browser
@@ -15,7 +25,12 @@ let isLoggedIn=false
 // undefined => value is not assigned yet
 // symbol => usnique
 
+//Non Primitives
+//Array,Objects,Functions
+
 
 console.log(typeof "tridib") // string
 console.log(typeof null); // object
 console.log(typeof undefined); // undefined
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
