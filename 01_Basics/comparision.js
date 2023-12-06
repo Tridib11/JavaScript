a9d68849f802fe8806fa3c5bf03ab351b51f27a2
Tridib Ghosh <86187166+Tridib11@users.noreply.github.com>
2023-12-06 17:26:55 +0000

    more lines added in comaprision

--- 01_Basics/comparision.js
+++ 01_Basics/comparision.js
@@ -10,3 +10,10 @@ The reason is that an equality check == and comparisons > <>= <= work differentl
 Comparisons convert null to a number, treating it as 0.
 That's why (3) null >=0 is true and (1) null > 0 is false.
 */
+
+console.log(undefined==0);  
+/* Anything with undefined is false*/
+
+// === (Strict check)
+
+console.log("2"===2);
