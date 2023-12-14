fcae57bf2ce6febf2f43fc041e0d5b9858d0a8d1
Tridib Ghosh <86187166+Tridib11@users.noreply.github.com>
2023-12-14 15:48:41 +0000

    File cleaner added

---
+++ Cohort/FileCleaner.js
@@ -0,0 +1,32 @@
+
+
+function clean(data){
+    var arr=data.split(" ")
+    var answerArray=[];
+    for(var i=0;i<arr.length;i++){
+        if(arr[i].length===0){
+            continue;
+        }else{
+            answerArray.push(arr[i]);
+        }
+    }
+    var answerString=answerArray.join(" ")
+    return answerString
+
+}
+//reading part
+const fs=require('fs')
+const file="/workspaces/JavaScript/Cohort/cleaner.txt"
+function readingfile(err,data){
+    if(err){
+        console.error(err)
+        return;
+    }
+    let cleanData=clean(data);
+    fs.writeFile(file,cleanData,'utf8',fileWritten);
+}
+fs.readFile(file,"utf8",readingfile)
+
+function fileWritten(){
+    console.log("Operation Successfull");
+}
