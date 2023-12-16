661b1d26eef66c95c5eb973a07d88ffec6a1222c
Tridib Ghosh <86187166+Tridib11@users.noreply.github.com>
2023-12-16 17:19:05 +0000

    HTTP server express added

--- Week_2_Backend/index.js
+++ Week_2_Backend/index.js
@@ -1,9 +1,29 @@
-const fs=require("fs");
-const express=require("express");
-function callbackFn(err,data){
-    console.log(data);
+//creating HTTP Server
+const express = require('express')
+const app = express()
+const port = 3000
+
+function sum(counter){
+    var sum=0;
+    for(var i=0;i<=counter;i++){
+        sum+=i;
+    }
+    return sum;
 }
-fs.readFile("a.txt","utf-8",callbackFn);
+
+function handleFirstRequest(req,res){
+    var counter=req.query.counter;//query counter
+    var calsulatedSum=sum(counter);//query counter
+    console.log(calsulatedSum);
+    var answer="The sum is "+calsulatedSum;
+    res.send(answer);
+}
+app.get('/handleSum', handleFirstRequest)
+
+function started(){
+    console.log("Example app listening on port "+port);
+}
+app.listen(port, started)
 
 
 
@@ -14,13 +34,24 @@ fs.readFile("a.txt","utf-8",callbackFn);
 
 
 
-// function sum(counter){
-//     var sum=0;
-//     for(var i=0;i<counter;i++){
-//         sum+=i;
-//     }
-//     return sum;
+
+
+
+
+
+// const fs=require("fs");
+// const express=require("express");
+// function callbackFn(err,data){
+//     console.log(data);
 // }
+// fs.readFile("a.txt","utf-8",callbackFn);
 
-// var calsulatedSum=sum(100);
-// console.log(calsulatedSum);
+
+
+
+
+
+
+
+
+
