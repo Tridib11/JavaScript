36cdad6c4ef8bb2e48aeaf30a2ebab3d91977a78
Tridib11 <tridibghosh12345@gmail.com>
2023-12-22 01:58:47 +0530

    idk what happend but some files added

---
+++ Js_Basics/Strings.js
@@ -0,0 +1,10 @@
+const name="Tridib"
+const repo=50
+console.log(`Hello my name is ${name} and my repo cout is ${repo}`); //String interpolation
+
+//another way to implement String is
+const gameName=new String('Tridib')
+console.log(gameName[0]);
+console.log(gameName.__proto__); //{}
+
+console.log(gameName.length);
