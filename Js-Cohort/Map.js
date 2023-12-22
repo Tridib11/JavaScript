6e1f49857c650d545fdab1aa917824b3958fa12c
Tridib11 <tridibghosh12345@gmail.com>
2023-12-22 16:57:37 +0530

    Modified map.js

--- Js-Cohort/Map.js
+++ Js-Cohort/Map.js
@@ -1,3 +1,16 @@
-const array=[1,2,3,4,5]
-const map1=array.map(x=>x+1)
-console.log(map1);
+// const array=[1,2,3,4,5]
+// const map1=array.map(x=>x+1)
+// console.log(map1);
+
+
+
+function higherOrder(arr,callback){
+    return callback(arr)
+}
+function usingMapFunction(arr){
+    const map1=arr.map(x=>x+1);
+    return map1;
+}
+const arr=[1,2,3,4,5]
+var answer=higherOrder(arr,usingMapFunction);
+console.log(answer);
