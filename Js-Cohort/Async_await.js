4cafb8aa9f0aa136d9ddcc1d5e1eae6f4e9cf868
Tridib11 <tridibghosh12345@gmail.com>
2023-12-26 23:49:44 +0530

    Async await file added

---
+++ Js-Cohort/Async_await.js
@@ -0,0 +1,28 @@
+//Normal promise syntax
+// function AsyncFunction(){
+//     let p=new Promise(function(resolve){
+//         resolve("hi there")
+//     })
+//     return p
+// }
+// function main(){
+//     AsyncFunction().then(function(value){
+//         console.log(value);
+//     })
+// }
+// main();
+
+
+//Async/Await Syntax
+
+function AsyncFunction(){
+    let p=new Promise(function(resolve){
+        resolve("hi there")
+    })
+    return p
+}
+async function main(){
+    const val=await AsyncFunction()
+    console.log(val);
+    }
+main();
