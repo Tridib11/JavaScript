466c84194c8ca38fa8e7d296d53d4237aa60e05f
Tridib11 <tridibghosh12345@gmail.com>
2023-12-30 23:41:03 +0530

    files modified

--- Backend/testing.js
+++ Backend/testing.js
@@ -1,6 +1,23 @@
 const express=require("express")
 const app=express()
+var bodyParser=require('body-parser')
 const port=3000
+
+let numberOfRequests=0;
+
+//applying middleware
+
+function middleware(req,res,next){
+    // console.log("From inside middleware "+req.headers.counterh);
+    numberOfRequests+=1
+    // console.log("Requests "+numberOfRequests);
+    // res.send("Error from middlware")
+    next()
+}
+
+// app.use(middleware)
+app.use(bodyParser.json())// bod parser middleware
+
 function calculateSum(num){
     sum=0
     for(let i=0;i<=num;i++){
@@ -10,13 +27,15 @@ function calculateSum(num){
 }
 
 function test(req,res){
-    var counter=req.query.counterh;
+    console.log(req.body);
+    // console.log(req.headers);
+    var counter=req.headers.counterh;
     // let answer=calculateSum(100)
     var answer=calculateSum(counter)
     res.send(`The calculated sum is ${answer}`)
 }
-app.get('/',test)
-
+//app.get('/',test)
+app.post('/',test)
 function portTesting(){
     console.log(`Listning on port ${port}`);
 }
