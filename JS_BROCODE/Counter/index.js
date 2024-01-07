b13c11b2199ba4340f3c431cf1d39cd7fec03900
Tridib11 <tridibghosh12345@gmail.com>
2024-01-08 03:44:05 +0530

    Count file added

---
+++ JS_BROCODE/Counter/index.js
@@ -0,0 +1,26 @@
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = function () {
+    if (count === 0) {
+        count = 1;
+    } else {
+        count++;
+    }
+    countLabel.textContent = count;
+};
+
+resetBtn.onclick = function () {
+    count = 0;
+    countLabel.textContent = count;
+};
+
+decreaseBtn.onclick = function () {
+    if (count > 0) {
+        count--;
+        countLabel.textContent = count;
+    }
+};
