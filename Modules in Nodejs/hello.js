ea1f5623f05ec8b69d86513600a7e00454e750d9
Tridib11 <tridibghosh12345@gmail.com>
2024-01-10 23:29:14 +0530

    Modules of Node js files added

---
+++ Modules in Nodejs/hello.js
@@ -0,0 +1,7 @@
+const math=require("./math")
+// console.log(math);
+// console.log(math.sub(2,4));
+// console.log(add(2,5));
+
+
+console.log(math.add(2,3));
---
+++ Modules in Nodejs/math.js
@@ -0,0 +1,19 @@
+/* Singular export function */
+
+// function add(a,b){
+//     return a+b
+// }
+// function sub(a,b){
+//     return a-b
+// }
+// module.exports={
+//     add,
+//     sub
+// }
+
+//or
+
+/* Multiple export function */
+
+exports.add=(a,b)=>a+b
+exports.sub=(a,b)=>a-b
