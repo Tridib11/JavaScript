e8f68d68c86c63e7c1ac5d27b4a0ee357895adf7
Tridib11 <tridibghosh12345@gmail.com>
2024-01-14 05:09:27 +0530

    files modified

--- TestingNodeHTTPServer/index.js
+++ TestingNodeHTTPServer/index.js
@@ -4,19 +4,24 @@ const url=require('url')
 
 const myServer=http.createServer((req,res)=>{
     if(req.url=="/favicon.ico") return
-    
     const myUrl=url.parse(req.url,true)
     const username=myUrl.query.myname;
     const id=myUrl.query.id
     const search=myUrl.query.search
     console.log(myUrl);
-    const logs=` ${username}, Id : ${id}, Searched ${search} \n`
+    var logs=` ${username}, Id : ${id}, Searched ${search} \n`
     fs.appendFile("./logs.txt",logs,(err,data)=>{
         switch(myUrl.pathname){
             case '/' : res.end("Homepage")
             break
             case '/about' : res.end(`Hey ${username}, Id : ${id}, Searched ${search} `)
             break
+            case '/results': 
+            const search=myUrl.query.search_query
+            logs=`Youtube url Search:Here are your results for ${search} \n`
+            fs.appendFile("./logs.txt",logs,(err,data)=>{})
+            res.end(`Here are your results for ${search}`)
+            break;
             default : res.end("404 Not Found")
         }
     })
