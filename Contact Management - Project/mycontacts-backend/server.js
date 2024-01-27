cab492b1799eac4c6e837ee2ebe56443b87e2fc1
Tridib11 <tridibghosh12345@gmail.com>
2024-01-27 22:01:12 +0530

    FIles added

--- Contact Management - Project/mycontacts-backend/server.js
+++ Contact Management - Project/mycontacts-backend/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandle");
-// const dotenv=require("dotenv").config()
+const connectDb = require("./config/dbConnection");
+const dotenv=require("dotenv").config()
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5001;
 
+connectDb()
+ 
 app.use(express.json())
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler)
+
 app.listen(port, () => {
   console.log(`Server running on the port ${port}`);
 });
