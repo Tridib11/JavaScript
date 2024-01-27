4bfc3afed26a6126fe3d32ef159d8ec2233bebf8
Tridib11 <tridibghosh12345@gmail.com>
2024-01-27 22:08:28 +0530

    Files added

--- Contact Management - Project/mycontacts-backend/controllers/userController.js
+++ Contact Management - Project/mycontacts-backend/controllers/userController.js
@@ -1,29 +1,31 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 
-const registerUser = asyncHandler(async (req,res)=>{
-    res.json({message:"Register the user"})
+const registerUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Register the user" });
 });
 
 //@desc Login a user
 //@route POST /api/users/login
 //@access public
 
-const loginUser = asyncHandler(async (req,res)=>{
-    res.json({message:"Login user"})
+const loginUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Login user" });
 });
 
-//@desc Current user
+//@desc Current user info
 //@route POST /api/users/current
-//@access public
+//@access private
 
-const currentUser = asyncHandler(async (req,res)=>{
-    res.json({message:"Current user information"})
+const currentUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Current user information" });
 });
 
-module.exports={
-    registerUser,
-    loginUser,
-    currentUser,
-}
+module.exports = {
+  registerUser,
+  loginUser,
+  currentUser,
+};
