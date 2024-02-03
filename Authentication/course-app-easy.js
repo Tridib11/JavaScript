6f91f57423eace2606c84e33ccf043f5777febc3
Tridib11 <tridibghosh12345@gmail.com>
2024-02-03 13:14:51 +0530

    All admin files added

--- Authentication/course-app-easy.js
+++ Authentication/course-app-easy.js
@@ -1,18 +1,64 @@
-const express=require("express")
-const app=express()
-app.use(express.json())
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-let ADMINS=[];
-let USERS=[];
-let COURSES=[];
+let ADMINS = [];
+let USERS = [];
+let COURSES = [];
 
-const adminAuthentication=(req,res,next)=>{
-    const {username,password}=req.headers
+const adminAuthentication = (req, res, next) => {
+  const { username, password } = req.headers;
+  const admin = ADMINS.find(
+    (a) => a.username === username && a.password === password
+  );
+  if (admin) {
+    next();
+  } else {
+    res.status(403).json({ message: "Admin authentication falied" });
+  }
+};
+app.post("/admin/signup", (req, res) => {
+  const admin = req.body;
+  const existingAdmin = ADMINS.find((a) => a.username === admin.username);
+  if (existingAdmin) {
+    res.status(403).json({ message: "Admin already exists" });
+  } else {
+    ADMINS.push(admin);
+    res.json({ message: "Admin created successfully" });
+  }
+});
 
-    const admin=ADMIN.find(a=>a.username===username && a.password===password)
-    if(admin){
-        next()
-    }else{
-        res.status(403).json({message:"Admin authentication falied"})
-    }
-}
+app.post("/admin/login", adminAuthentication, (req, res) => {
+  res.json({ message: "Logged in successfully" });
+});
+
+app.post("/admin/courses", adminAuthentication, (req, res) => {
+  const course = req.body;
+  if (!course.title || !course.description) {
+    return res
+      .status(411)
+      .json({ message: "Please give the title and the description :)" });
+  }
+  course.id = Date.now();
+  COURSES.push(course);
+  res.json({ message: "Course created succesfully", courseId: course.id });
+});
+
+app.put("/admin/courses/:courseId", adminAuthentication, (req, res) => {
+  const courseId = parseInt(req.params.courseId);
+  const course = COURSES.find(c => c.id === courseId);
+  if (course) {
+    Object.assign(course, req.body);
+    res.json({ message: "Course updated successfully" });
+  } else {
+    res.status(404).json({ message: "Course not found" });
+  }
+});
+
+app.get('/admin/courses', adminAuthentication, (req, res) => {
+    res.json({ courses: COURSES });
+  });
+
+app.listen(3000, () => {
+  console.log("Server started at port 3000");
+});
