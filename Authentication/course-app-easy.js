97fdcdc81bbdbc9ddf43ed13ac49921c5d6a69c8
Tridib11 <tridibghosh12345@gmail.com>
2024-02-03 12:13:22 +0530

    Files added

--- Authentication/course-app-easy.js
+++ Authentication/course-app-easy.js
@@ -7,5 +7,12 @@ let USERS=[];
 let COURSES=[];
 
 const adminAuthentication=(req,res,next)=>{
-    
+    const {username,password}=req.headers
+
+    const admin=ADMIN.find(a=>a.username===username && a.password===password)
+    if(admin){
+        next()
+    }else{
+        res.status(403).json({message:"Admin authentication falied"})
+    }
 }
