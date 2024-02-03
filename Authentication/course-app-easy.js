b1dd8937e59c3619169819ab032406f18b8a6869
Tridib11 <tridibghosh12345@gmail.com>
2024-02-03 14:38:40 +0530

    FILES ADDED

--- Authentication/course-app-easy.js
+++ Authentication/course-app-easy.js
@@ -17,6 +17,23 @@ const adminAuthentication = (req, res, next) => {
     res.status(403).json({ message: "Admin authentication falied" });
   }
 };
+
+const userAuthentication = (req, res, next) => {
+  const { username, password } = req.headers;
+  const user = USERS.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (user) {
+    req.user = user; // Add user object to the request
+    next();
+  } else {
+    res.status(403).json({ message: "User authentication failed" });
+  }
+};
+
+
+//-----------------ADMIN ROUTES----------------------------
+
 app.post("/admin/signup", (req, res) => {
   const admin = req.body;
   const existingAdmin = ADMINS.find((a) => a.username === admin.username);
@@ -46,7 +63,7 @@ app.post("/admin/courses", adminAuthentication, (req, res) => {
 
 app.put("/admin/courses/:courseId", adminAuthentication, (req, res) => {
   const courseId = parseInt(req.params.courseId);
-  const course = COURSES.find(c => c.id === courseId);
+  const course = COURSES.find((c) => c.id === courseId);
   if (course) {
     Object.assign(course, req.body);
     res.json({ message: "Course updated successfully" });
@@ -55,9 +72,49 @@ app.put("/admin/courses/:courseId", adminAuthentication, (req, res) => {
   }
 });
 
-app.get('/admin/courses', adminAuthentication, (req, res) => {
-    res.json({ courses: COURSES });
-  });
+app.get("/admin/courses", adminAuthentication, (req, res) => {
+  res.json({ courses: COURSES });
+});
+
+///---------------------------USERS ROUTES------------------------------
+
+app.post("/users/signup", (req, res) => {
+  // const user={...req.body,purchasedCourses:[]}
+  //or
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+    purchasedCourses: [],
+  };
+  USERS.push(user);
+  res.json({ message: "User created successfully" });
+});
+
+app.post("/users/login", userAuthentication, (req, res) => {
+  res.json({ message: "Logged in successfully" });
+});
+
+app.get("/users/courses", userAuthentication, (req, res) => {
+  // COURSES.filter(c => c.published)
+  let filteredCourses = [];
+  for (let i = 0; i < COURSES.length; i++) {
+    if (COURSES[i].published) {
+      filteredCourses.push(COURSES[i]);
+    }
+  }
+  res.json({ courses: filteredCourses });
+});
+
+app.post("/users/courses/:courseId", userAuthentication, (req, res) => {
+  const courseId = Number(req.params.courseId);
+  const course = COURSES.find((c) => c.id === courseId && c.published);
+  if (course) {
+    req.user.purchasedCourses.push(courseId);
+    res.json({ message: "Course purchased successfully" });
+  } else {
+    res.status(404).json({ message: "Course not found or not available" });
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server started at port 3000");
