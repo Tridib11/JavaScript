a5d5958960d1a3f45c22e8225177d59bee22d241
Tridib11 <tridibghosh12345@gmail.com>
2024-02-04 14:00:27 +0530

    files added

---
+++ 3.1_new/zod_n_email.js
@@ -0,0 +1,42 @@
+/*
+{
+  email:string=>email
+  password:atleast 8 characters
+  country : "IN","US"
+}
+*/
+const express=require("express");
+const zod=require("zod");
+
+const app=express()
+
+
+app.use(express.JSON())
+
+function validateInput(obj){
+
+const schema2 = zod.object({
+  email: zod.string().email(),
+  password: zod.string().min(8),
+  country: zod.union([zod.literal("IN"), zod.literal("US")])
+})
+  const response=schema2.safeParse(obj)
+  console.log(response)
+}
+
+app.post("/ogin",(req,res)=>{
+  const response=validateInput(req.body)
+  if(!response){
+    res.json({
+      msg:"Your inputs are invalid"
+    })
+    return;
+  }
+})
+
+// validateInput({
+//   email:"ejeyd@example.com",
+//   password:"12345678",
+//   country:"IN"
+// })
+
