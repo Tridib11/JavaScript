4dbcedbab0edce0841d681e7583d96f8a5319e72
Tridib11 <tridibghosh12345@gmail.com>
2024-02-05 23:13:58 +0530

    code added

--- 3.2_Authentication/index.js
+++ 3.2_Authentication/index.js
@@ -2,6 +2,8 @@ const express=require("express")
 const jwt=require("jsonwebtoken")
 const app=express()
 
+const jwtPassword="123456";
+
 const allUsers=[
     {
         username:"tridib@mail.com",
@@ -15,8 +17,35 @@ const allUsers=[
     }
 ]
 function userExists(username,password){
-    
+
 }
+
+app.post('/signin',(req,res)=>{
+    const username=req.body.username
+    const password=req.body.password
+    if(!userExists){
+        return res.status(404).json({
+            message:"User doesn't exists"
+        })
+    }
+    var token=jwt.sign({username : username},jwtPassword) //encrypts the username
+    return res.json({
+        token,
+    })
+})
+
+app.get('/users',(req,res)=>{
+    const token=req.headers.authorization;
+    try{
+        const decoded=jwt.verify(token,jwtPassword)
+        const username=decoded.username;
+    }
+    catch(err){
+        return res.status(403).json({
+            msg:"Invalid token",
+        })
+    }
+})
 
 app.listen(3000,()=>{
     console.log("Server started at port 3000")
