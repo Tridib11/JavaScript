cac116619b9c04478af8ecf663861d2fd246dd46
Tridib11 <tridibghosh12345@gmail.com>
2024-02-05 21:16:57 +0530

    files added

--- 3.1_new/emailvalidate_zod.js
+++ 3.1_new/emailvalidate_zod.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const zod=require("zod");
+const zod = require("zod");
 const app = express();
 
-app.use(express.json())
-
+app.use(express.json());
 
 /*
 {
@@ -16,19 +15,17 @@ app.use(express.json())
 const schema2 = zod.object({
   email: zod.string().email(),
   password: zod.string().min(8),
-  country: zod.union([zod.literal("IN"), zod.literal("US")])
+  country: zod.union([zod.literal("IN"), zod.literal("US")]),
 });
 
-
-const schema=zod.array(zod.number())
-
+const schema = zod.array(zod.number());
 
 app.post("/health-checkup", (req, res) => {
   const kidneys = req.body.kidneys;
-  const resposne=schema.safeParse(kidneys)
+  const resposne = schema.safeParse(kidneys);
   res.send({
-    resposne
-  })
+    resposne,
+  });
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
