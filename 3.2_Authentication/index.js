00f7c2e0d4b546d7d261eaca9c2de6ea9ae4a83c
Tridib11 <tridibghosh12345@gmail.com>
2024-02-05 23:46:16 +0530

    find function added

--- 3.2_Authentication/index.js
+++ 3.2_Authentication/index.js
@@ -1,52 +1,67 @@
-const express=require("express")
-const jwt=require("jsonwebtoken")
-const app=express()
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
 
-const jwtPassword="123456";
+const jwtPassword = "123456";
 
-const allUsers=[
-    {
-        username:"tridib@mail.com",
-        password:"123",
-        name:"tridib",
-    },
-    {
-        username:"raman@mail.com",
-        password:"234",
-        name:"raman",
-    }
-]
-function userExists(username,password){
+const allUsers = [
+  {
+    username: "tridib@mail.com",
+    password: "123",
+    name: "tridib",
+  },
+  {
+    username: "raman@mail.com",
+    password: "234",
+    name: "raman",
+  },
+];
+// function userExists(username, password) {
+//   const userExists = false;
+//   for (let i = 0; i < allUsers.length; i++) {
+//     if (
+//       allUsers[i].username === username &&
+//       allUsers[i].password === password
+//     ) {
+//       userExists = true;
+//     }
+//   }
+//   return userExists;
+// }
 
-}
+//--or--
 
-app.post('/signin',(req,res)=>{
-    const username=req.body.username
-    const password=req.body.password
-    if(!userExists){
-        return res.status(404).json({
-            message:"User doesn't exists"
-        })
-    }
-    var token=jwt.sign({username : username},jwtPassword) //encrypts the username
-    return res.json({
-        token,
-    })
-})
+function userExists(username, password) {
+    return allUsers.find(user => user.username === username && user.password === password);
+  }
+  
 
-app.get('/users',(req,res)=>{
-    const token=req.headers.authorization;
-    try{
-        const decoded=jwt.verify(token,jwtPassword)
-        const username=decoded.username;
-    }
-    catch(err){
-        return res.status(403).json({
-            msg:"Invalid token",
-        })
-    }
-})
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!userExists) {
+    return res.status(404).json({
+      message: "User doesn't exists",
+    });
+  }
+  var token = jwt.sign({ username: username }, jwtPassword); //encrypts the username
+  return res.json({
+    token,
+  });
+});
 
-app.listen(3000,()=>{
-    console.log("Server started at port 3000")
-})
+app.get("/users", (req, res) => {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+  } catch (err) {
+    return res.status(403).json({
+      msg: "Invalid token",
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server started at port 3000");
+});
