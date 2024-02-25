c5ca069e5f398f7e0586375c85630e72c33e31c0
Tridib11 <tridibghosh12345@gmail.com>
2024-02-26 01:52:35 +0530

    Files Added

--- Week-6/6.1/week-6-experiments/src/App.jsx
+++ Week-6/6.1/week-6-experiments/src/App.jsx
@@ -1,22 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <Header title="Tridib1"></Header>
+      <HeaderWithButton/>
+      <Header title="Tridib2"></Header>
+      <Header title="Tridib2"></Header>
       <Header title="Tridib2"></Header>
     </div>
-  )
+  );
 }
 
-function Header({title}){
-  return <div>
-    {title}
-  </div>
+function HeaderWithButton() {
+  const [title, setTitle] = useState("My name is Tridib");
+  function updateTitle() {
+    setTitle("My name is " + Math.random());
+  }
+  return (
+    <div>
+      <button onClick={updateTitle}>Update the title</button>
+      <Header title={title}></Header>
+    </div>
+  );
 }
 
-export default App
+function Header({ title }) {
+  return <div>{title}</div>;
+}
+
+export default App;
