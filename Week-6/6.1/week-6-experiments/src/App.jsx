cd52a87440fdddfd47b1e78dce9f0af6402f9bfe
Tridib11 <tridibghosh12345@gmail.com>
2024-02-26 02:40:08 +0530

    Files Modifed

--- Week-6/6.1/week-6-experiments/src/App.jsx
+++ Week-6/6.1/week-6-experiments/src/App.jsx
@@ -5,30 +5,14 @@ import viteLogo from "/vite.svg";
 
 
 function App() {
-const [title,setTitle]=useState("My name is Tridib")
-function updateTitle(){
-  setTitle("My name is "+Math.random())
-}  
+
 return (
     <div>
-      <button onClick={updateTitle}>Update</button>
-      <Header title={title}></Header>
-      <Header title="hello world"></Header>
-      <Header title="hello world"></Header>
-      <Header title="hello world"></Header>
-      <Header title="hello world"></Header>
-      <Header title="hello world"></Header>
+    
       
     </div>
   );
 }
 
-
-
-const Header=React.memo(function Header({title}){
-  return <div>
-    {title}
-  </div>
-})
 
 export default App;
