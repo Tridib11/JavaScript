f1a2c8b9ff2947e4e902c3f5d1cc2bbdb565bb50
Tridib11 <tridibghosh12345@gmail.com>
2024-02-26 02:20:16 +0530

    Files Modifed

--- Week-6/6.1/week-6-experiments/src/App.jsx
+++ Week-6/6.1/week-6-experiments/src/App.jsx
@@ -1,30 +1,34 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
+
+
 function App() {
-  return (
-    <div>
-      <HeaderWithButton></HeaderWithButton>
-      <Header title="hello world"></Header>
-    </div>
-  );
-}
-function HeaderWithButton() {
-  const [title, setTitle] = useState("My name is Tridib");
-  function updateTitle() {
-    setTitle("My name is " + Math.random());
-  }
-  return (
+const [title,setTitle]=useState("My name is Tridib")
+function updateTitle(){
+  setTitle("My name is "+Math.random())
+}  
+return (
     <div>
       <button onClick={updateTitle}>Update</button>
       <Header title={title}></Header>
+      <Header title="hello world"></Header>
+      <Header title="hello world"></Header>
+      <Header title="hello world"></Header>
+      <Header title="hello world"></Header>
+      <Header title="hello world"></Header>
+      
     </div>
   );
 }
 
-function Header({ title }) {
-  return <div>{title}</div>;
-}
+
+
+const Header=React.memo(function Header({title}){
+  return <div>
+    {title}
+  </div>
+})
 
 export default App;
