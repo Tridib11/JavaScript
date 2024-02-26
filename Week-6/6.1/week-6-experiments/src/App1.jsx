cb90861774e7d4a7ce54c1f4093a9d89021d37b7
Tridib11 <tridibghosh12345@gmail.com>
2024-02-27 03:57:11 +0530

    File added

--- Week-6/6.1/week-6-experiments/src/App1.jsx
+++ Week-6/6.1/week-6-experiments/src/App1.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useState } from "react";
 function App() {
+  const [title,setTitle]=useState("My name is Tridib")
+
+  function updateTitle(){
+    setTitle("My name is "+ Math.random())
+  }
   return <div>
-    <Header title="Tridib1"></Header>
+    <button onClick={updateTitle}>Update the title</button>
+    <Header title={title}></Header>
     <Header title="Tridib2"></Header>
   </div>;
 }
@@ -13,3 +20,39 @@ function Header({title}){
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// const Header = (props) => {
+//   const { title } = props;
+//   return <div>{title}</div>;
+// };
+
+// const Body = () => {
+//   return (
+//     <div>
+//       <Header title="Triidib 1" />
+//       <Header title="Triidib 2" />
+//     </div>
+//   );
+// };
+
+// const AppLayout = () => {
+//   return (
+//     <div className="app">
+//       <Header></Header>
+//       <Body></Body>
+//     </div>
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<AppLayout />);
