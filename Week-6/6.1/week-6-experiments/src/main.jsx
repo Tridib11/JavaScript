408abcefea2b10ab1c651a534051681d241f8a16
Tridib11 <tridibghosh12345@gmail.com>
2024-02-27 04:13:21 +0530

    FIle Modified

--- Week-6/6.1/week-6-experiments/src/main.jsx
+++ Week-6/6.1/week-6-experiments/src/main.jsx
@@ -1,46 +1,13 @@
-// import ReactDOM from 'react-dom/client'
-// import App from './App1.jsx'
-// import './index.css'
+import ReactDOM from 'react-dom/client'
+import App from './App1.jsx'
+import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 
-//     <App />
+    <App />
 
-// )
+)
 
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { useState } from "react";
-
-const Body = () => {
-  const [title,setTitle]=useState("My name is Tridib")
-  function Update(){
-    setTitle("My name is "+ Math.random())
-  }
-  return (
-    <div>
-      <button onClick={Update}>Click here to update</button>
-      <Header title={title} />
-      <Header title="Triidib 2" />
-    </div>
-  )
-};
-
-const Header = ({title}) => {
-  return <div>{title}</div>;
-};
-
-const AppLayout = () => {
-  return (
-    <div className="app">
-      <Header></Header>
-      <Body></Body>
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
 
 
