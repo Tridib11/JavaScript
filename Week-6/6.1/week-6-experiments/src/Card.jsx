846721691437fe430945feac31b7eb1571b21104
Tridib11 <tridibghosh12345@gmail.com>
2024-02-27 06:13:29 +0530

    File Modified

--- Week-6/6.1/week-6-experiments/src/Card.jsx
+++ Week-6/6.1/week-6-experiments/src/Card.jsx
@@ -1,9 +1,20 @@
 function App(){
 
+    return <div>
+        <CardWrapper innerComponent={<TextComponent/>} />
+    </div>
 }
 
-function cardWrapper({}){
+function CardWrapper({innerComponent}){
+    return <div style={{border:"2px solid black"}}>
+        {innerComponent}
+    </div>
 
+}
+function TextComponent(){
+    return <div>
+        Hi there
+    </div>
 }
 
 export default App
