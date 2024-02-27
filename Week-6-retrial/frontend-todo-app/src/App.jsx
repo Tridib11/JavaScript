3bf5443f2febca5ada6559efd228d9338403947e
Tridib11 <tridibghosh12345@gmail.com>
2024-02-28 02:13:34 +0530

    FIle Modified

---
+++ Week-6-retrial/frontend-todo-app/src/App.jsx
@@ -0,0 +1,47 @@
+// var todos=[{
+//   title:"go to the gym",
+//   description:"go to gum11",
+//   id:1
+// },{
+//   title:"go eat food",
+//   description:"go eat at x12",
+//   id:2
+// }
+// ]
+
+import { useState } from "react";
+
+let todo = {
+  title: "go to the gym",
+  description: "go to gum11",
+  id: 1,
+};
+
+// setInterval(()=>{
+//   todo.title="asdsded"
+// },1000)
+
+function App() {
+  const [todo, setTodo] = useState({
+    title: "go to the gym",
+    description: "go to gum11",
+    id: 1,
+  });
+
+  setInterval(() => {
+    setTodo({
+      title: "go walk",
+      description: "go for walk at 3",
+      id: 3,
+    });
+  }, 2000);
+
+  return (
+    <>
+      <h5>Hi there</h5>
+      {todo.title},{todo.description},{todo.id}
+    </>
+  );
+}
+
+export default App;
