0ff34698cf438725b6cdb1bfd368d16930e390e0
Tridib11 <tridibghosh12345@gmail.com>
2024-02-29 11:03:25 +0530

    File Modified

--- Week-6/6.2/week-6-2/src/App1.jsx
+++ Week-6/6.2/week-6-2/src/App1.jsx
@@ -1,5 +1,33 @@
-function App(){
+import { useEffect, useState } from "react"
+import axios from "axios"
 
+
+function App(){
+    return <div>
+        <Todo id={3}/>
+    </div>
 }
+
+function Todo({id}){
+    const [todo,setTodo]=useState({})
+
+    useEffect(()=>{
+        axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+        .then((response) =>{
+            setTodo(response.data.todo)
+        })
+    },[])
+    return <div>
+        <h1>
+            {todo.title},
+        </h1>
+        <h4>
+            {todo.description}
+        </h4>
+    </div>
+}
+
+
+
 
 export default App
