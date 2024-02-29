13a3819f85988bd35fbaa74d379f89eefb7e0bb0
Tridib11 <tridibghosh12345@gmail.com>
2024-02-29 12:23:04 +0530

    File Modified

---
+++ Week-6/6.2/week-6-2/src/App3.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function App() {
+  const [inputValue, setInputValue] = useState(1);
+  const [counter, setCounter] = useState(0);
+  let count = 0;
+  for (let i = 1; i <= inputValue; i++) {
+    count += i;
+  }
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={function (e) {
+          setInputValue(e.target.value);
+        }}
+        placeholder={"Find sum from 1 to n"}
+      />
+      <br />
+      Sum from 1 to {inputValue} is {count}
+      <br />
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Counter ({counter})
+      </button>
+    </div>
+  );
+}
+
+export default App;
