9b1e3474242331490c872f3ec1e8666cad666033
Tridib11 <tridibghosh12345@gmail.com>
2024-03-02 17:36:33 +0530

    File Modified

--- Week-6/6.2/week-6-2/src/App3.jsx
+++ Week-6/6.2/week-6-2/src/App3.jsx
@@ -1,30 +1,27 @@
 import React, { useMemo, useState } from "react";
-import { debounce, throttle } from 'lodash';
+import { debounce, throttle } from "lodash";
 
 function App() {
   const [inputvalue, setInputValue] = useState(1);
   var [counter, setCounter] = useState(0);
-  
+
   let count = useMemo(() => {
     let finalCount = 0;
     for (let i = 0; i < inputvalue; i++) {
-        finalCount += i;
+      finalCount += i;
     }
     return finalCount;
   }, [inputvalue]);
   return (
     <div>
       <input
-  type="text"
-  onChange={debounce(function (e) {
-    setInputValue(e.target.value);
-  }, 300)} // 300ms debounce delay
-  placeholder="Sum of number from 1 to n"
-/>
-
-
-{   
-/*
+        type="text"
+        onChange={debounce(function (e) {
+          setInputValue(e.target.value);
+        }, 300)} // 300ms debounce delay
+        placeholder="Sum of number from 1 to n"
+      />
+      {/*
   With debouncing, the event handler will only be invoked after the specified delay (300ms in this case) after the last input event. Throttling, on the other hand, ensures that the event handler is called at most once per specified interval (300ms in this case), regardless of how many times the event is fired during that interval. Choose the one that fits your use case best.
 
 
@@ -38,7 +35,6 @@ function App() {
   placeholder="Sum of number from 1 to n"
 />
 */}
-
       <br />
       Sum of number from 1 to {inputvalue} is {count}
       <br />
