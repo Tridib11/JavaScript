ca4be4fcaaa620229d0972f58c71781ce79deaf9
Tridib11 <tridibghosh12345@gmail.com>
2024-03-02 09:49:12 +0530

    Files modief

---
+++ Week-6/6.2/week-6-2/src/App5.jsx
@@ -0,0 +1,25 @@
+import { useState,memo } from "react";
+
+function App(){
+    const [count,setCount]=useState(0)
+
+    function inputFunction(){
+        console.log("Hi there");
+    }
+    return <div>
+        <ButtonComponent inputFunction={inputFunction/>
+        <button onClick={()=>{
+            setCount(count+1)
+        }}>Clicked me ({count})</button>
+    </div>
+}
+
+const ButtonComponent=memo(()=>{
+    console.log("child rendered");
+    return <div>
+        <button>Button Clicked</button>
+    </div>
+})
+
+
+export default App;
