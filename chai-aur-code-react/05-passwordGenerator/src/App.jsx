35c508af931b432b5b70207a8a4b5f25a0d0dfdc
Tridib11 <tridibghosh12345@gmail.com>
2024-03-04 04:01:01 +0530

    File added

--- chai-aur-code-react/05-passwordGenerator/src/App.jsx
+++ chai-aur-code-react/05-passwordGenerator/src/App.jsx
@@ -1,11 +1,15 @@
 import { useCallback } from "react";
-import { useState,useEffect } from "react";
+import { useState,useEffect,useRef } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState();
+
+  //useRef hook
+
+  const passwordRef=useRef(null)
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
@@ -26,7 +30,6 @@ function App() {
 
   useEffect(()=>{
     passwordGenerator()
-
   },[length, numberAllowed, charAllowed, passwordGenerator])
 
   return (
@@ -41,6 +44,7 @@ function App() {
               className="outline-none w-full py-1 px-3"
               placeholder="Password"
               readOnly
+              ref={passwordRef}
             />
             <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
               Copy
