51e24cfafc4b9c4d7fdeea187cc203ad8eff8610
Tridib11 <tridibghosh12345@gmail.com>
2024-03-03 09:53:34 +0530

    File added

---
+++ chai-aur-code-react/02Counter/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ const[count,setCount]=useState(0)
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value {count}</h2>
+      <button onClick={()=>{
+        setCount(count+1)
+      }}>Add value</button>
+      <br />
+      <button onClick={()=>{
+        setCount(count-1)
+      }}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
