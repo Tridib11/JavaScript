5293714c164db8604edd4be8ea422e950e149372
Tridib11 <tridibghosh12345@gmail.com>
2024-03-04 03:30:40 +0530

    File added

--- chai-aur-code-react/05-passwordGenerator/src/App.jsx
+++ chai-aur-code-react/05-passwordGenerator/src/App.jsx
@@ -43,9 +43,40 @@ function App() {
           </div>
           <div className="flex text-sm gap-x-2">
             <div className="flex items-center gap-x-1">
-              <input type="range" min={6} max={100} value={length} className="cursor-pointer" />
+              <input
+                type="range"
+                min={6}
+                max={100}
+                value={length}
+                onChange={(e) => {
+                  setLength(event.target.value);
+                }}
+                className="cursor-pointer"
+              />
+              <label>Length {length}</label>
             </div>
-
+            <div className="flex items-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={numberAllowed}
+                id="numberInput"
+                onChange={() => {
+                  setNumberAllowed((prev) => !prev);
+                }}
+              />
+              <label htmlFor="numberInput">Numbers</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={charAllowed}
+                id="characterInput"
+                onChange={() => {
+                  setCharAllowed((prev) => !prev);
+                }}
+              />
+              <label htmlFor="characterInput">Characters</label>
+            </div>
           </div>
         </div>
       </div>
