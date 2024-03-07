71721737d3b3aa71e959cb977435ae6eb527a937
Tridib11 <tridibghosh12345@gmail.com>
2024-03-07 11:29:59 +0530

    File Added

--- useRefHook/src/App.jsx
+++ useRefHook/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const a=useRef(0)
+  useEffect(()=>{
+    a.current=a.current+1
+    console.log(`rendering and the value of a is ${a.current}`);
+  })
 
   return (
     <>
