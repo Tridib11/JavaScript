8138d242dad9fd3ec3392a8b3cce4e4248b3e4f5
Tridib11 <tridibghosh12345@gmail.com>
2024-03-07 10:28:58 +0530

    File Added

--- useEffecthook/src/App.jsx
+++ useEffecthook/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(()=>{
+    alert("Hey welcome to my page")
+  },[])
 
+  useEffect(()=>{
+    alert("COunt was changed")
+  },[count])
   return (
     <>
       <div>
