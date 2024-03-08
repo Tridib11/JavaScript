18ad59839e153a0ebb9888e9d10ce725a4f3ee62
Tridib11 <tridibghosh12345@gmail.com>
2024-03-08 13:32:22 +0530

    file added

---
+++ handlingevents/solutons-18/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+//https://jsonplaceholder.typicode.com/posts
+function App() {
+  
+  return (
+    <div>
+      <div className="container">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
