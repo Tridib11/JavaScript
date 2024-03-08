2c340855fc3a68d3f8bfc8eeae51c925b0f427ea
Tridib11 <tridibghosh12345@gmail.com>
2024-03-08 19:42:58 +0530

    File added

--- handlingevents/solutons-18/src/App.jsx
+++ handlingevents/solutons-18/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 //https://jsonplaceholder.typicode.com/posts
 function App() {
-  
+  const[cards,setCards]=useState([])
+
+  const fetchData=async()=>{
+    let a=await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data=await a.json()
+    setCards(data)
+    console.log(data)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+    
   return (
     <div>
       <div className="container">
-
+        {cards.map((card)=>{
+          return <div className="card">
+            <h1>{card.title}</h1>
+            <p>{card.body}</p>
+            <span>By: UserId: {card.userId}</span>
+          </div>
+        })}
       </div>
     </div>
   )
