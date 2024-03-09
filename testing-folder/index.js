ee2a1b07536f526c4aff64c228935ab99f970b20
Tridib11 <tridibghosh12345@gmail.com>
2024-03-09 22:20:03 +0530

    FIle added

--- testing-folder/index.js
+++ testing-folder/index.js
@@ -9,7 +9,14 @@ let COURSES = [];
 
 // Admin routes
 app.post("/admin/signup", (req, res) => {
-  // logic to sign up admin
+  const admin = req.body;
+  const existingAdmin = ADMINS.find((a) => a.username === admin.username);
+  if (existingAdmin) {
+    res.status(403).json({ message: "Admin already exists" });
+  } else {
+    ADMINS.push(admin);
+    res.json("Admin created successfully");
+  }
 });
 
 app.post("/admin/login", (req, res) => {
