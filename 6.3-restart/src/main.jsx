02282de9513022227e9580978f759de892e2215a
Tridib11 <tridibghosh12345@gmail.com>
2024-03-10 23:26:29 +0530

    File added

--- 6.3-restart/src/main.jsx
+++ 6.3-restart/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './ict.jsx'
+import App from './useMemo_testing.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
