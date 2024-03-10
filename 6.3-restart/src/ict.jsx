0c8f86d6fa7a4037a6d7ea7040995be8c966b132
Tridib11 <tridibghosh12345@gmail.com>
2024-03-10 22:55:31 +0530

    File added

--- 6.3-restart/src/ict.jsx
+++ 6.3-restart/src/ict.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function ict() {
   const [exchangeData, setExchangeData] = useState({});
   const [bankData, setBankData] = useState({});
-  fetch("https://google.com", async (res) => {
-    const json = await res.json();
-    setBankData({ income: 100 });
-    //Assume it is  {income : 100}
-  });
-
-  setTimeout(() => {
-    setExchangeData({
-      returns: 100,
-    });
-  }, 1000);
+  console.log("Hi there rerender");
+  useEffect(() => {
+    setTimeout(() => {
+      setBankData({ income: 100 });
+    }, 3000);
+  }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setExchangeData({
+        returns: 100,
+      });
+    }, 1000);
+  }, []);
 
   const incomeTax = (bankData.income + exchangeData.returns) * 0.3;
 
