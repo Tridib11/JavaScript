4d22f7f8952dbc9761ec30ab2a86314ab10d1741
Tridib11 <tridibghosh12345@gmail.com>
2024-03-10 23:26:31 +0530

    File added

--- 6.3-restart/src/useMemo_testing.jsx
+++ 6.3-restart/src/useMemo_testing.jsx
@@ -22,12 +22,17 @@ function useMemo() {
       setBankData({
         income:100
       })
-    })
+    },5000)
   },[])
 
+
+  console.log("Hi there before")
+  const cryptoReturns=exchange1Data.returns+exchange2Data.returns
+  console.log("Hi there after")
+  const incomeTax=(cryptoReturns+bankData.income)*0.3
   return (
     <div>
-      
+      hi there your income tax returns are {incomeTax}
     </div>
   )
 }
