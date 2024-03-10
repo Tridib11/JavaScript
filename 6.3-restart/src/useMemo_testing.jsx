d37992a33aa6ef24db10deaabbe49dc9b4ea25e3
Tridib11 <tridibghosh12345@gmail.com>
2024-03-10 23:31:13 +0530

    File added

--- 6.3-restart/src/useMemo_testing.jsx
+++ 6.3-restart/src/useMemo_testing.jsx
@@ -1,44 +1,38 @@
-import React, { useEffect, useState,useMemo } from 'react'
+import React, { useEffect, useState, useMemo } from "react";
 
 function useMemo_testing() {
-  const[exchange1Data,setExchange1Data]=useState({})
-  const[exchange2Data,setExchange2Data]=useState({})
-  const[bankData,setBankData]=useState({})
+  const [exchange1Data, setExchange1Data] = useState({});
+  const [exchange2Data, setExchange2Data] = useState({});
+  const [bankData, setBankData] = useState({});
 
-  useEffect(()=>{
+  useEffect(() => {
     setExchange1Data({
-      returns:100
-    })
-  },[])
+      returns: 100,
+    });
+  }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     setExchange2Data({
-      returns:100
-    })
-  },[])
+      returns: 100,
+    });
+  }, []);
 
-  useEffect(()=>{
-    setTimeout(()=>{
+  useEffect(() => {
+    setTimeout(() => {
       setBankData({
-        income:100
-      })
-    },5000)
-  },[])
+        income: 100,
+      });
+    }, 5000);
+  }, []);
 
+  const cryptoReturns = useMemo(() => {
+    console.log("Hi there before");
+    return exchange1Data.returns + exchange2Data.returns;
+  }, [exchange1Data, exchange2Data]);
 
-
-  const cryptoReturns=useMemo(()=>{
-    console.log("Hi there before")
-    return exchange1Data.returns+exchange2Data.returns
-  },[exchange1Data,exchange2Data])
-
-  console.log("Hi there after")
-  const incomeTax=(cryptoReturns+bankData.income)*0.3
-  return (
-    <div>
-      hi there your income tax returns are {incomeTax}
-    </div>
-  )
+  console.log("Hi there after");
+  const incomeTax = (cryptoReturns + bankData.income) * 0.3;
+  return <div>hi there your income tax returns are {incomeTax}</div>;
 }
 
-export default useMemo_testing
+export default useMemo_testing;
