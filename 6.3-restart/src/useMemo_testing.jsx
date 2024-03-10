163025977486d50d16f65535357ed023ab842b10
Tridib11 <tridibghosh12345@gmail.com>
2024-03-10 23:18:23 +0530

    File added

--- 6.3-restart/src/useMemo_testing.jsx
+++ 6.3-restart/src/useMemo_testing.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function useMemo() {
+  const[exchange1Data,setExchange1Data]=useState({})
+  const[exchange2Data,setExchange2Data]=useState({})
+  const[bankData,setBankData]=useState({})
+
+  useEffect(()=>{
+    setExchange1Data({
+      returns:100
+    })
+  },[])
+
+  useEffect(()=>{
+    setExchange2Data({
+      returns:100
+    })
+  },[])
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setBankData({
+        income:100
+      })
+    })
+  },[])
+
   return (
     <div>
       
