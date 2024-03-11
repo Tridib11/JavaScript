91763ede9d1b0535e7a47d4b0041915c2071160a
Tridib11 <tridibghosh12345@gmail.com>
2024-03-11 15:03:23 +0530

    File added

--- react_restart/useMemo_testing/src/ReactMemoComponent/ResultComponent.jsx
+++ react_restart/useMemo_testing/src/ReactMemoComponent/ResultComponent.jsx
@@ -1,9 +1,14 @@
-import React from "react"
-const ResultCompoenent=({marks})=>{
-    console.log("Inside result component")
-    return (
-        <h2>Result is : {marks}</h2>
-    )
-}
+import React from "react";
+const ResultCompoenent = ({ marks, subject }) => {
+  console.log("Inside result component");
+  const percentageMarks = (80 * 100) / 100;
+  return (
+    <div>
+      <h2>Result is : {marks}</h2>
+      <h2>Percentage is : {percentageMarks}</h2>
+      <h2>For subject : {subject}</h2>
+    </div>
+  );
+};
 
-export default React.memo(ResultCompoenent)
+export default React.memo(ResultCompoenent);
