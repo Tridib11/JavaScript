ed2240a831c9781ce4bacea1b6bbe407bb52d3c6
Tridib11 <tridibghosh12345@gmail.com>
2024-03-12 01:50:51 +0530

    File Added

---
+++ week-7/week-7.1/src/LazyLoading.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import React, { lazy } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import Dashboard from lazy(()=> "./components/Dashboard");
+import Landing from lazy(()=>"./components/Landing");
+
+
+function App() {
+
+  return (
+    <div>  
+      <BrowserRouter>
+      <Appbar/>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+// navigate works only inside a component
+function Appbar(){
+  const navigate=useNavigate()
+  return <div>
+        <button onClick={()=>{
+          navigate("/")
+        }}>Landing Page</button>
+        <button onClick={()=>{
+          navigate("/dashboard")
+        }}>Dashboard</button>
+      </div>
+}
+
+export default App;
