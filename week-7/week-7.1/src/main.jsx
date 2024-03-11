60fbc45f739e1e54c1609234a191a2bf1a9f5b89
Tridib11 <tridibghosh12345@gmail.com>
2024-03-12 02:46:30 +0530

    File Added

--- week-7/week-7.1/src/main.jsx
+++ week-7/week-7.1/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './LazyLoading.jsx'
+import App from './Example.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
