abd6c58babd9c40a12d53559f44d0793e48c4431
Tridib11 <tridibghosh12345@gmail.com>
2024-03-12 00:51:11 +0530

    File Added

--- week-7/week-7.1/src/App.jsx
+++ week-7/week-7.1/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
-
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import Landing from './components/Landing'
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/' element={<Landing/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
