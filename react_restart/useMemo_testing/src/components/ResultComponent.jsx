a15ae1373194768bebb961d1a68aa29fb01ce645
Tridib11 <tridibghosh12345@gmail.com>
2024-03-11 12:39:28 +0530

    File added

--- react_restart/useMemo_testing/src/components/ResultComponent.jsx
+++ react_restart/useMemo_testing/src/components/ResultComponent.jsx
@@ -1,8 +1,10 @@
-const ResultCompoenent=({marks})=>{
-    console.log("Inside result component")
-    return (
-        <h2>Result is : {marks}</h2>
-    )
-}
+import React, { useMemo } from "react";
+const ResultCompoenent = ({ marks }) => {
+  const memoizedResult = useMemo(() => {
+    console.log("Inside result component");
+    return <h2>Result is : {marks}</h2>;
+  },[marks]);
+  return memoizedResult
+};
 
-export default ResultCompoenent
+export default ResultCompoenent;
