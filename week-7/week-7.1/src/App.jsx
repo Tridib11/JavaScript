0305eff625934e700da12aa3f6cfcd7da1c0214f
Tridib11 <tridibghosh12345@gmail.com>
2024-03-12 01:30:36 +0530

    File Added

--- week-7/week-7.1/src/App.jsx
+++ week-7/week-7.1/src/App.jsx
@@ -3,25 +3,37 @@ import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Landing from "./components/Landing";
 
-
 function App() {
-  const navigate=useNavigate()
   return (
     <div>
-      <div>
-        <button onClick={()=>{
-          navigate("/")
-        }}>Landing Page</button>
-        <button onClick={()=>{
-          navigate("/dashboard")
-        }}>Dashboard</button>
-      </div>
       <BrowserRouter>
+        <Appbar />
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/" element={<Landing />} />
         </Routes>
       </BrowserRouter>
+    </div>
+  );
+}
+function Appbar() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Landing Page
+      </button>
+      <button
+        onClick={() => {
+          navigate("/dashboard");
+        }}
+      >
+        Dashboard
+      </button>
     </div>
   );
 }
