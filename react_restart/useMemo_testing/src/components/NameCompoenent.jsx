4f31b4533668d3df62cb1cd00687e0c99383929e
Tridib11 <tridibghosh12345@gmail.com>
2024-03-11 12:39:31 +0530

    File added

--- react_restart/useMemo_testing/src/components/NameCompoenent.jsx
+++ react_restart/useMemo_testing/src/components/NameCompoenent.jsx
@@ -1,8 +1,10 @@
-const NameCompoenent=({Name})=>{
-    console.log("Inside Name component")
-    return (
-        <h2>Name is : {Name}</h2>
-    )
-}
+import React, { useMemo } from "react";
+const NameCompoenent = ({ Name }) => {
+  const memoizedName = useMemo(() => {
+    console.log("Inside Name component");
+    return <h2>Name is : {Name}</h2>;
+  }, [Name]);
+  return memoizedName
+};
 
-export default NameCompoenent
+export default NameCompoenent;
