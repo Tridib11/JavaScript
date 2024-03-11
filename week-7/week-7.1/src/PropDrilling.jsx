231914385afecfeb7671ee2e7a4528b977720615
Tridib11 <tridibghosh12345@gmail.com>
2024-03-12 02:48:02 +0530

    File Added

---
+++ week-7/week-7.1/src/PropDrilling.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+function App() {
+    const [count,setCount]=useState(0)
+  return (
+    <div>
+        <Count/>
+        <Buttons/>
+    </div>
+  )
+}
+
+function Count(){
+
+}
+
+function Buttons(){
+
+}
+
+export default App
