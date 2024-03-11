2f368965422a41fc30cccd3788fdb16d31107bcc
Tridib11 <tridibghosh12345@gmail.com>
2024-03-11 15:12:29 +0530

    File added

--- react_restart/useMemo_testing/src/ReactMemoComponent/ResultComponent.jsx
+++ react_restart/useMemo_testing/src/ReactMemoComponent/ResultComponent.jsx
@@ -1,14 +1,30 @@
-import React from "react";
-const ResultCompoenent = ({ marks, subject }) => {
-  console.log("Inside result component");
-  const percentageMarks = (80 * 100) / 100;
-  return (
-    <div>
-      <h2>Result is : {marks}</h2>
-      <h2>Percentage is : {percentageMarks}</h2>
-      <h2>For subject : {subject}</h2>
-    </div>
-  );
-};
+// import React from "react"
+// const ResultCompoenent=({marks,subject})=>{
+//     console.log("Inside result component")
+//     return (
+//         <h2>Result is : {marks}</h2>
+//     )
+// }
 
-export default React.memo(ResultCompoenent);
+// export default React.memo(ResultCompoenent)
+
+
+
+import React,{useMemo} from "react"
+const ResultCompoenent=({marks,subject})=>{
+    console.log("Inside Result component")
+
+    const percentageMarks=useMemo(()=>{
+        return (80*100)/100
+    },[])
+    return (
+        <div>
+
+            <h2>Result is : {marks}</h2>
+            <h2>Percentage is : {percentageMarks}</h2>
+            <h2>For subject : {subject}</h2>
+        </div>
+    )
+}
+
+export default React.memo(ResultCompoenent)
