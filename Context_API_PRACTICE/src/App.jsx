879c94e6ee7c087c35831d941fbde07f299ee72e
Tridib11 <tridibghosh12345@gmail.com>
2024-03-12 15:35:29 +0530

    File added

--- Context_API_PRACTICE/src/App.jsx
+++ Context_API_PRACTICE/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import './App.css'
+import { CountContext } from './context'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(7)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <CountContext.Provider value={count}>
+      <CountRenderer/>
+      </CountContext.Provider>
+      <Increaser setCount={setCount}/>
+    </div>
+  )
+}
+
+function CountRenderer(){
+  const count=useContext(CountContext)
+  return <div>
+    {count}
+  </div>
+}
+
+function Increaser({setCount}){
+  return(
+    <div>
+      <button onClick={()=>{
+        setCount(count=>count+1)
+      }}></button>
+    </div>
   )
 }
 
