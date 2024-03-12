a41b0c70d7b7620ab387fff7fe02b5263b6ce924
Tridib11 <tridibghosh12345@gmail.com>
2024-03-13 00:34:07 +0530

    File added

--- week-7/week-7.2/src/App.jsx
+++ week-7/week-7.2/src/App.jsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { CountContext } from "./Context";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
 
 function App() {
   return (
@@ -13,35 +13,48 @@ function Count() {
   console.log("Count re-rendered");
   return (
     <div>
-      <CountRenderer />
-      <Buttons setCount={setCount} />
+      <RecoilRoot>
+        <CountRenderer />
+        <br /><br />
+        <Buttons />
+      </RecoilRoot>
     </div>
   );
 }
 
 function CountRenderer() {
-  const count = 0;
+  const count = useRecoilValue(countAtom);
   return (
     <div>
       <b>{count}</b>
+      <EvenCountRenderer/>
     </div>
   );
 }
 
+function EvenCountRenderer(){
+  const isEven=useRecoilValue(evenSelector)
+  return <div>
+    {isEven ? "It is even":null}
+  </div>
+
+}
+
 function Buttons() {
-  const count = 0;
+  const setCount = useSetRecoilState(countAtom);
+  console.log("Buttons rerendered");
   return (
     <div>
       <button
         onClick={() => {
-          setCount(count + 1);
+          setCount(count=>count + 1);
         }}
       >
         Increase
       </button>
       <button
         onClick={() => {
-          setCount(count - 1);
+          setCount(count=>count - 1);
         }}
       >
         Decrease
