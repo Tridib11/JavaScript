32b6948fa093ca1878c453a84ac3d368dd6e46a4
Tridib11 <tridibghosh12345@gmail.com>
2024-03-12 16:01:04 +0530

    File Added

--- week-7/week-7.2/src/App.jsx
+++ week-7/week-7.2/src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from 'react'
+import { useState,useContext } from 'react'
 import './App.css'
+import { CountContext } from './Context'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount}/>
+      </CountContext.Provider>
     </>
+  )
+}
+
+function Count({setCount}){
+  return <div>
+    <CountRenderer/>
+    <Buttons setCount={setCount}/>
+  </div>
+}
+
+function CountRenderer(){
+  const count=useContext(CountContext)
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({setCount}){
+  const count=useContext(CountContext)
+  return (
+    <div>
+      <button onClick={()=>{
+        setCount(count=>count+1)
+      }}>
+        Increase
+      </button>
+    </div>
   )
 }
 
