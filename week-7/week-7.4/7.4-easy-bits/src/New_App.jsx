e90a9ca638108abce567315ed094d9685158f0b8
Tridib11 <tridibghosh12345@gmail.com>
2024-03-13 18:57:40 +0530

    File Added

--- week-7/week-7.4/7.4-easy-bits/src/New_App.jsx
+++ week-7/week-7.4/7.4-easy-bits/src/New_App.jsx
@@ -1,17 +1,19 @@
 
 import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot, useRecoilValue } from 'recoil';
 import { todosAtomFamily } from './atoms_n';
 
 function App() {
   return <RecoilRoot>
     <Todo id={1}/>
     <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
   </RecoilRoot>
 }
 
 function Todo({id}) {
-   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+   const todo = useRecoilValue(todosAtomFamily(id));
 
   return (
     <>
