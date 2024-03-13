830e4d8fd74244b9c2716a25a6ba86cb4b8a089e
Tridib11 <tridibghosh12345@gmail.com>
2024-03-13 22:48:13 +0530

    File Added

--- week-7/week-7.4/7.4-easy-bits/src/Selector_family.jsx
+++ week-7/week-7.4/7.4-easy-bits/src/Selector_family.jsx
@@ -1,18 +1,21 @@
 
 import './App.css'
-import { RecoilRoot, useRecoilValue } from 'recoil';
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil';
 import { todosAtomFamily } from './selector_atom';
 
 function App() {
   return <RecoilRoot>
     <Todo id={1}/>
     <Todo id={2} />
-
+    <Todo id={2} />
+    <Todo id={2} />
   </RecoilRoot>
 }
 
 function Todo({id}) {
+
    const todo=useRecoilValue(todosAtomFamily(id))
+   console.log(todo);
 
   return (
     <>
