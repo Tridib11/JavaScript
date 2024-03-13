e82c84efec28ace978837fed70209eb0da444711
Tridib11 <tridibghosh12345@gmail.com>
2024-03-13 17:44:16 +0530

    File Added

---
+++ week-7/week-7.4/7.4-easy-bits/src/New_App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import { RecoilRoot, useRecoilState } from 'recoil';
+import { todosAtomFamily } from './atoms_n';
+
+function App() {
+  return <RecoilRoot>
+    <Todo id={1}/>
+    <Todo id={2} />
+  </RecoilRoot>
+}
+
+function Todo({id}) {
+   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+
+  return (
+    <>
+      {todo.title}
+      {todo.description}
+      <br />
+    </>
+  )
+}
+
+export default App
