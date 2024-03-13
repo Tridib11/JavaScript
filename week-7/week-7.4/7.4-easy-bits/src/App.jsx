e36236c5f6ae0ccd66e535b653ad221ba6ed0c1e
Tridib11 <tridibghosh12345@gmail.com>
2024-03-13 15:42:06 +0530

    File Added

--- week-7/week-7.4/7.4-easy-bits/src/App.jsx
+++ week-7/week-7.4/7.4-easy-bits/src/App.jsx
@@ -1,5 +1,5 @@
 import { RecoilRoot, useRecoilValue } from "recoil";
-import { networkAtom } from "./Atoms";
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom } from "./Atoms";
 
 function App() {
  return <RecoilRoot>
@@ -9,15 +9,18 @@ function App() {
 
 function MainApp(){
   const networkNotificationCount=useRecoilValue(networkAtom)
+  const jobsAtomCount=useRecoilValue(jobsAtom)
+  const notificationAtomCount=useRecoilValue(notificationAtom)
+  const messagingAtomCount=useRecoilValue(messagingAtom)
 
   return (
     <div>
       <button>Home</button>
 
       <button>My network ({networkNotificationCount>=100?"99+":networkNotificationCount})</button>
-      <button>Jobs ()</button>
-      <button>Messaging ()</button>
-      <button>Notifications ()</button>
+      <button>Jobs ({jobsAtomCount})</button>
+      <button>Messaging ({notificationAtomCount})</button>
+      <button>Notifications ({messagingAtomCount})</button>
 
       <button>Me</button>
     </div>
