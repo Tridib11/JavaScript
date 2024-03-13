770717a0cf5fe85cb8f843dadfc37538d0e1b0ce
Tridib11 <tridibghosh12345@gmail.com>
2024-03-14 03:24:33 +0530

    File added

--- week-8/8.2/tailwind-prac/src/NewApp.jsx
+++ week-8/8.2/tailwind-prac/src/NewApp.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function NewApp() {
   return (
     // <div className='bg-red-500 md:bg-blue-500'>
-    //   hi there      
+    //   hi there
     // </div>
-
-    <div className='grid grid-cols-1 md:grid-cols-3'>
-        <div className='bg-red-500 '>Child 1</div>
-        <div className='bg-blue-500 '>Child 2</div>
-        <div className='bg-green-500 '>Child 3</div>
-    </div>
-
-    
-  )
-}
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="bg-red-500 ">Child 1</div>
+        <div className="bg-blue-500 ">Child 2</div>
+        <div className="bg-green-500 ">Child 3</div>
+      </div>
+    </>
+  );
+} 
+  
