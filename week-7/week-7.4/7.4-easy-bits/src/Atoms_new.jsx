01cbddbd47cbb0493cdb6baca44a03f00c6c0fc3
Tridib11 <tridibghosh12345@gmail.com>
2024-03-13 17:35:24 +0530

    File Added

--- week-7/week-7.4/7.4-easy-bits/src/Atoms_new.jsx
+++ week-7/week-7.4/7.4-easy-bits/src/Atoms_new.jsx
@@ -1,10 +1,16 @@
+import axios from "axios";
 import { atom, selector } from "recoil";
 
 export const notifications = atom({
     key: "networkAtom",
-    default: {}
-
-    });
+    default: selector({
+        key:"networkAtomSelector",
+        get:async()=>{
+            const res=await axios.get("https://sum-server.100xdevs.com/notifications")
+            return res.data
+        }
+    })
+});
 
 export const totalNotificationSelector = selector({
     key: "totalNotificationSelector",
