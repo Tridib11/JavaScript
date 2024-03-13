0dca39ecf80e4810a7a6b1ca66e0d2b9e1670329
Tridib11 <tridibghosh12345@gmail.com>
2024-03-14 00:30:05 +0530

    File added

--- week-8/8.2/tailwind-prac/src/App.jsx
+++ week-8/8.2/tailwind-prac/src/App.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-     <div style={{display:"flex", justifyContent:'center'}}>
-      <div style={{backgroundColor:"red"}}>hi</div>
-      <div style={{backgroundColor:"green"}}>hi</div>
-      <div style={{backgroundColor:"yellow"}}>hi</div>
-     </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ backgroundColor: "red" }}>hi</div>
+        <div style={{ backgroundColor: "green" }}>hi</div>
+        <div style={{ backgroundColor: "yellow" }}>hi</div>
+      </div>
+      <br />
+      <br />
 
-     <div className='flex'>
-      <div style={{backgroundColor:"red"}}>hi</div>
-      <div style={{backgroundColor:"green"}}>hi</div>
-      <div style={{backgroundColor:"yellow"}}>hi</div>
-     </div>
+      <div className="flex justify-center">
+        <div className="bg-red-500">hi</div>
+        <div className="bg-green-500">hi</div>
+        <div className="bg-blue-500">hi</div>
+      </div>
+
+      <br />
+      <br />
+
+      <div className="grid grid-cols-10  ">
+        <div className="bg-red-500 col-span-4">hi</div>
+        <div className="bg-green-500 col-span-4">hi</div>
+        <div className="bg-blue-500 col-span-2">hi</div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
