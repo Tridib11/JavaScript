c4f355447e13cd0a612df89fe03770bf070dfe09
Tridib11 <tridibghosh12345@gmail.com>
2024-03-14 00:31:11 +0530

    File added

--- week-8/8.2/tailwind-prac/src/App.jsx
+++ week-8/8.2/tailwind-prac/src/App.jsx
@@ -6,14 +6,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <div style={{ backgroundColor: "red" }}>hi</div>
-        <div style={{ backgroundColor: "green" }}>hi</div>
-        <div style={{ backgroundColor: "yellow" }}>hi</div>
-      </div>
-      <br />
-      <br />
-
       <div className="flex justify-center">
         <div className="bg-red-500">hi</div>
         <div className="bg-green-500">hi</div>
