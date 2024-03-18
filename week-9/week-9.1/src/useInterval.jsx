242404b8a0fb46f21e077438f362fff96794eda6
Tridib11 <tridibghosh12345@gmail.com>
2024-03-19 02:03:45 +0530

    File added

---
+++ week-9/week-9.1/src/useInterval.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useInterval(() => {
+    setCount(c => c + 1);
+  }, 1000)
+
+  return (
+    <>
+      Timer is at {count}
+    </>
+  )
+}
+
+export default App
