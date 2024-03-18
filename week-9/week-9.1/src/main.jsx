c1f064af3e676c736d85359a4a4713e530368a3e
Tridib11 <tridibghosh12345@gmail.com>
2024-03-19 02:11:53 +0530

    File added

--- week-9/week-9.1/src/main.jsx
+++ week-9/week-9.1/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './useMousePointer.jsx'
+import App from './useInterval.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
