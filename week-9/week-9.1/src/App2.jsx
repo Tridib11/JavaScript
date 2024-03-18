bcd3d161481551339a3c574cb165c8affc39185f
Tridib11 <tridibghosh12345@gmail.com>
2024-03-18 23:50:47 +0530

    File added

--- week-9/week-9.1/src/App2.jsx
+++ week-9/week-9.1/src/App2.jsx
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from 'react';
 function MyComponent() {
   useEffect(() => {
     // Perform setup or data fetching here
-
+    console.log("Component mounted");
     return () => {
       // Cleanup code (similar to componentWillUnmount)
+      console.log("Compoenent Unmounted");
     };
   }, []);
 
